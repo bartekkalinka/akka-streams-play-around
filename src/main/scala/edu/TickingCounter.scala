9e50b790b82e1a3b89685c23db2ba73cfe1db2a7
Bartek Kalinka <bartkali@hotmail.com>
2015-12-01 17:23:19 +0100

    conflate demo

--- src/main/scala/edu/TickingCounter.scala
+++ src/main/scala/edu/TickingCounter.scala
@@ -1,33 +1,44 @@
 package edu
 
 import akka.actor.{ActorSystem, Cancellable}
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{ZipWith, FlowGraph, Source}
+import akka.stream.{ActorMaterializerSettings, ActorMaterializer}
+import akka.stream.scaladsl.{Flow, ZipWith, FlowGraph, Source}
 import scala.concurrent.duration._
 
 object TickingCounter {
-  def counter: Source[Long, Unit] = zipWithTick(Source(Stream.iterate(0L)(_ + 1)))
+  def slowTick: Source[Unit, Cancellable] = Source(0 seconds, 1 seconds, ())
 
-  def constantTick: Source[Unit, Cancellable] = Source(0 seconds, 1 seconds, ())
-
-  def zipWithTick[A](toZip: Source[A, Unit]): Source[A, Unit] = {
+  def zipWithTick[A](tick: Source[Unit, Cancellable], toZip: Source[A, Unit]): Source[A, Unit] = {
     Source() { implicit builder: FlowGraph.Builder[Unit] =>
       import FlowGraph.Implicits._
       val zip = builder.add(ZipWith[Unit, A, A]((a: Unit, i: A) => i))
-      constantTick ~> zip.in0
+      tick ~> zip.in0
       toZip ~> zip.in1
       zip.out
     }
   }
 
+  def counter: Source[Long, Unit] = zipWithTick(slowTick, Source(Stream.iterate(0L)(_ + 1)))
+
   def sourceAccumulation[A](source: Source[A, Unit]): Source[String, Unit] =
     counter.scan("")((a, b) => a + b.toString)
 
+  def pickWithTick[A](tick: Source[Unit, Cancellable], source: Source[A, Unit]): Source[A, Unit] = {
+    val takeLast: Source[A, Unit] = source.conflate(x => x)((acc, elem) => elem)
+    zipWithTick(tick, takeLast)
+  }
+
+  def fasterCounter: Source[Long, Unit] = zipWithTick(Source(0 seconds, 0.2 seconds, ()), Source(Stream.iterate(0L)(_ + 1)))
+  
+  def fasterDropped: Source[Long, Unit] = pickWithTick(slowTick, fasterCounter)
+  
   def run = {
     implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
+    val settings = ActorMaterializerSettings(system).withInputBuffer(1, 1)
+    implicit val materializer = ActorMaterializer(settings)
 
-    sourceAccumulation(counter).runWith(Utils.myLoggingSink)
+    fasterDropped.runWith(Utils.myLoggingSink)
+    //sourceAccumulation(counter).runWith(Utils.myLoggingSink)
     //counter.runWith(Utils.myLoggingSink)
   }
 }
