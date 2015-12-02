f7f05425a214cb59864a23796bca44875de49df2
Bartek Kalinka <bartkali@hotmail.com>
2015-12-02 16:04:10 +0100

    detached stage demo

--- src/main/scala/edu/TickingCounter.scala
+++ src/main/scala/edu/TickingCounter.scala
@@ -4,6 +4,22 @@ import akka.actor.{ActorSystem, Cancellable}
 import akka.stream.{ActorMaterializer, Attributes}
 import akka.stream.scaladsl.{ZipWith, FlowGraph, Source}
 import scala.concurrent.duration._
+import akka.stream.stage._
+
+class OptionPipe[T]() extends DetachedStage[T, Option[T]] {
+  private var currentValue: Option[T] = None
+
+  override def onPush(elem: T, ctx: DetachedContext[Option[T]]): UpstreamDirective = {
+    currentValue = Some(elem)
+    ctx.pull()
+  }
+
+  override def onPull(ctx: DetachedContext[Option[T]]): DownstreamDirective = {
+    val result = ctx.push(currentValue)
+    currentValue = None
+    result
+  }
+}
 
 object TickingCounter {
   def slowTick: Source[Unit, Cancellable] = Source(0 seconds, 1 seconds, ())
@@ -20,27 +36,34 @@ object TickingCounter {
     }
   }
 
-  def counter: Source[Long, Unit] = zipWithTick(slowTick, Source(Stream.iterate(0L)(_ + 1)))
+  def slowCounter: Source[Long, Unit] = zipWithTick(slowTick, Source(Stream.iterate(0L)(_ + 1)))
 
   def sourceAccumulation[A](source: Source[A, Unit]): Source[String, Unit] =
-    counter.scan("")((a, b) => a + b.toString)
+    slowCounter.scan("")((a, b) => a + b.toString)
 
   def pickWithTick[A](tick: Source[Unit, Cancellable], source: Source[A, Unit]): Source[A, Unit] = {
     val takeLast: Source[A, Unit] = source.conflate(x => x)((acc, elem) => elem)
     zipWithTick(tick, takeLast)
   }
 
-  def fasterCounter: Source[Long, Unit] = zipWithTick(Source(0 seconds, 0.2 seconds, ()), Source(Stream.iterate(0L)(_ + 1)))
+  def optionPipe[A](tick: Source[Unit, Cancellable], source: Source[A, Unit], default: A): Source[Option[A], Unit] = {
+    val pipedSource = source.transform(() => new OptionPipe[A]())
+    zipWithTick(tick, pipedSource)
+  }
+
+  def fasterTick: Source[Unit, Cancellable] = Source(0 seconds, 0.2 seconds, ())
+
+  def fasterCounter: Source[Long, Unit] = zipWithTick(fasterTick, Source(Stream.iterate(0L)(_ + 1)))
   
   def fasterDropped: Source[Long, Unit] = pickWithTick(slowTick, fasterCounter)
+
+  def slowThroughFaster: Source[Option[Long], Unit] = optionPipe(fasterTick, slowCounter, 999L)
   
   def run = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
 
-    fasterDropped.runWith(Utils.myLoggingSink)
-    //sourceAccumulation(counter).runWith(Utils.myLoggingSink)
-    //counter.runWith(Utils.myLoggingSink)
+    slowThroughFaster.runWith(Utils.myLoggingSink)
   }
 }
 
